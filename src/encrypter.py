75ed9a090ffc3ad00861ff5914cc7a9afceefeb7
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2025-05-03 21:05:06 -0300

    refactor(encrypter): enhance file encryption and decryption logic with async support

--- src/encrypter.py
+++ src/encrypter.py
@@ -26,45 +26,78 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from concurrent.futures import ThreadPoolExecutor
-from pathlib import Path
-
+import sys
+from anyio import open_file
+from anyio import Path as aPath
+from io import DEFAULT_BUFFER_SIZE
 from cryptography.fernet import Fernet
 
 
-def gen_key() -> bytes:
-    return Fernet.generate_key()
+class Encrypter:
 
+    def __init__(self, fernet: Fernet) -> None:
+        self.__fernet = fernet
 
-def get_fernet(key: str | bytes) -> Fernet:
-    if isinstance(key, str):
-        key = key.encode()
-    return Fernet(key)
+    @staticmethod
+    def gen_key() -> bytes:
+        return Fernet.generate_key()
 
+    @staticmethod
+    def get_fernet(key: str | bytes) -> Fernet:
+        if isinstance(key, str):
+            key = key.encode()
+        return Fernet(key)
 
-def encrypt_bytes(fernet: Fernet, data: bytes) -> bytes:
-    return fernet.encrypt(data)
+    @classmethod
+    def from_key(cls, key: str | bytes) -> "Encrypter":
+        if isinstance(key, str):
+            key = key.encode()
+        return cls(Fernet(key))
 
+    async def parse_files(self, files: list[aPath], decrypt: bool = False) -> None:
+        if not decrypt:
+            for _file in files:
+                await self.encrypt_file(_file)
+        else:
+            for _file in files:
+                await self.decrypt_file(_file)
 
-def decrypt_bytes(fernet: Fernet, data: bytes) -> bytes:
-    return fernet.decrypt(data)
+    async def encrypt_file(self, path: aPath, buff: int = DEFAULT_BUFFER_SIZE) -> None:
+        temp_f = path.with_name(path.suffix + ".temp")
+        # fmt: off
+        async with await open_file(path, "rb") as f_in, await open_file(temp_f, "wb") as f_out:
+            # fmt: on
+            try:
+                while True:
+                    chunk = await f_in.read(buff)
+                    if not chunk:
+                        break
+                    encrypted = self.__fernet.encrypt(chunk)
+                    await f_out.write(len(encrypted).to_bytes(4, sys.byteorder)) # 32 bits integer
+                    await f_out.write(encrypted)
+                await temp_f.replace(path)
+            except (PermissionError, FileNotFoundError, IsADirectoryError) as e:
+                print(f"Skipping: {path}: {e}")
 
+    async def decrypt_file(self, path: aPath) -> None:
+        temp_f = path.with_name(path.suffix + ".temp")
+        # fmt: off
+        async with await open_file(path, "rb") as f_in, await open_file(temp_f, "wb") as f_out:
+            # fmt: on
+            try:
+                while True:
+                    size_bytes = await f_in.read(4)
+                    if not size_bytes:
+                        break
 
-def write_encrypted(file: Path, data: bytes, suffix: str = ".locked") -> None:
-    file.write_bytes(data)
-    if not file.name.endswith(suffix):
-        file.rename(file.with_name(file.name + suffix))
+                    size = int.from_bytes(size_bytes)
+                    encrypted_chunk = await f_in.read(size)
+                    if len(encrypted_chunk) != size:
+                        raise ValueError(f"Corrupted File: expected {size} bytes, got {len(encrypted_chunk)}")
 
-
-def encrypt_file(fernet: Fernet, file: Path) -> None:
-    file_bytes = file.read_bytes()
-    encrypted_bytes = encrypt_bytes(fernet, file_bytes)
-    try:
-        write_encrypted(file, encrypted_bytes)
-    except (PermissionError, FileNotFoundError, IsADirectoryError) as e:
-        print(f"Skipping: {file}: {e}")
-
-
-def encrypt_files(fernet: Fernet, files: list[Path]) -> None:
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        executor.map(lambda f: encrypt_file(fernet, f), files)  # type: ignore
+                    plain_chunk = self.__fernet.decrypt(encrypted_chunk)
+                    await f_out.write(len(plain_chunk).to_bytes(4, sys.byteorder)) # 32 bits integer
+                    await f_out.write(plain_chunk)
+                await temp_f.replace(path)
+            except (PermissionError, FileNotFoundError, IsADirectoryError, ValueError) as e:
+                print(f"Skipping: {path}: {e}")
